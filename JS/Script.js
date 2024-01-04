590fe247dc46e715d8ec674400a988f1bf00ec91
RenaldiAnjasmara <Renaldi.14aldi@gmail.com>
2024-01-04 08:34:44 +0700

    jss area done

---
+++ JS/Script.js
@@ -0,0 +1,7 @@
+function area() {
+var height = document.getElementById("height1").value;
+var base = document.getElementById("base1").value;
+result1=0.5*(height*base);
+
+document.getElementById("result1").value=result1;
+}
